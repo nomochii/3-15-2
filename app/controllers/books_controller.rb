f9b54fd2213a5eeee9186fe3cd4f8f08d45e7cb3
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-06-01 15:12:26 +0000

    showがエラー中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+    flash[:notice] = "successfully"
      redirect_to book_path(@book.id)
     else
       render :new
@@ -17,8 +18,11 @@ class BooksController < ApplicationController
   end
 
   def show
-     @book = Book.find(params[:id])
+    @book = Book.new(book_params)
+    @book = Book.find(params[:id])
+    redirect_to book_path(@book.id)
   end
+
 
   def edit
     @book = Book.find(params[:id])
