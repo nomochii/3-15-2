db585779b26cf91f501fb1ea60ee66c7bda98aa3
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-06-04 18:44:15 +0000

    レイアウトが整ってない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
   	if @book.save
+  	  flash[:notice] = "Book was successfully created."
   		redirect_to book_path(@book.id)
   	else
   		render :new
@@ -36,7 +37,8 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path(book.id)
   end
 
   private
