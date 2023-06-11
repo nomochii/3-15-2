782cf65d7592e45291bf31277c9d7337ed728163
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-06-11 10:19:01 +0000

    [Finish]3-15(課題２）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books
+  #get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #patch 'books/:id' => 'books#update', as: 'update_book'
 end
